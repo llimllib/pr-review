29a55ddc57194b81a228191794edcf18ed08912b
Bill Mill <bill@billmill.org>
2026-03-11 08:56:20 -0400

    chore: remove package-lock.json
    we're using bun not npm

--- tools/release.sh
+++ tools/release.sh
@@ -37,7 +37,6 @@ fi
 # Update version in package.json
 sed -i.bak "s/\"version\": \"$CURRENT\"/\"version\": \"$NEW_VERSION\"/" package.json
 rm package.json.bak
-npm install
 
 # Run checks before committing
 echo "Running biome check..."
